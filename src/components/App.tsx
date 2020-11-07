28ac6d1467ab64c21f2b11a8c4c309ad5bab3560
Ken Honda <ken@MacBook-Pro.local>
2020-11-07 21:18:08 +0900

    [add] first commit!

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h4>React-TypeScript Framework</h4>
+      <p>This is a framework of React project with TypeScript.</p>
+    </>
+  );
+}
+
+export default App;
