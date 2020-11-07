c1c75d9a2df59d5a74a031a46979997a7450c06e
Ken Honda <ken@MacBook-Pro.local>
2020-11-08 00:06:58 +0900

    [add]mterial-ui button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const App = () => {
   return (
     <>
       <h4>React-TypeScript Framework</h4>
       <p>This is a framework of React project with TypeScript.</p>
+      <Button variant="contained" color="primary">sample button</Button>
     </>
   );
 }
